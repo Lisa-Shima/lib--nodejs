cdb6811cd712dc3d5a62e4755dba0fe0da29de5a
Lisa-Shima <shimalisa2@gmail.com>
2025-05-08 18:49:39 +0200

    Committing - Adding a new book

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,6 +2,7 @@ const AppDataSource = require('../config/data-source')
 
 const BOOK_REPO = AppDataSource.getRepository('Book')
 
+// fetching all books
 async function getAllBooks(req, res, next){
     try{
         const books = await BOOK_REPO.find()
@@ -12,6 +13,22 @@ async function getAllBooks(req, res, next){
     }
 }
 
+// creating a new book
+async function createBook(req, res, next){
+    try{
+        // const book = await BOOK_REPO.create(req.body)
+        const { title, author } = req.body
+        if(!title || !author) return res.status(404).json({message: 'Both title and author are required'})
+            const book = BOOK_REPO.create({title, author})
+        const result = await BOOK_REPO.save(book)
+        res.status(201).json(result)
+    }
+    catch(e){
+        next(e)
+    }
+}
+
 module.exports = {
-    getAllBooks
+    getAllBooks,
+    createBook
 }
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllBooks } = require('../controllers/bookController')
+const { getAllBooks, createBook } = require('../controllers/bookController')
 
 router.get('/', getAllBooks)
+router.post('/', createBook)
 
 module.exports = router
